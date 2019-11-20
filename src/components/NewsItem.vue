ae8db63fa3e8816df56325442a3a1f1f6017c87a
Naoto Imamachi <naoto.imamachi@gmail.com>
2019-11-20 12:31:11 +0900

    Created preliminary newsItem using the data of Bing News Search

--- src/components/NewsItem.vue
+++ src/components/NewsItem.vue
@@ -2,13 +2,30 @@
   <div id="newItem">
     <v-card class="mx-auto">
       <v-card-text>
-        <div>{{ news.at_published }}</div>
-        <p class="display-1 text--primary">{{ news.title }}</p>
-        <div class="text--primary">{{ news.text }}</div>
+        <div>{{ news.datePublished }}</div>
+        <div>
+          {{ news.provider[0]["0"].name }}
+        </div>
+
+        <v-row>
+          <v-col cols="2" align="center" justify="center">
+            <v-img
+              class="white--text align-end"
+              :height="100"
+              :width="100"
+              :src="news.image.thumbnail.contentUrl"
+            >
+            </v-img>
+          </v-col>
+          <v-col cols="10">
+            <p class="display-1 text--primary">{{ news.name }}</p>
+          </v-col>
+        </v-row>
+        <div class="text--primary">{{ news.description }}</div>
       </v-card-text>
       <v-card-actions>
         <a :href="news.url" target="_blank" rel="noopener noreferrer">
-          <v-btn text color="deep-purple accent-4">Learn More</v-btn>
+          <v-btn text color="deep-purple accent-4">Link</v-btn>
         </a>
       </v-card-actions>
     </v-card>
@@ -26,5 +43,9 @@ export default {
 <style scoped>
 a {
   text-decoration: none;
+}
+img {
+  width: 40px;
+  height: 40px;
 }
 </style>
